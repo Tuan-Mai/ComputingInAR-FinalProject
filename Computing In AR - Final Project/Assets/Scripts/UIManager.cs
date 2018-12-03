b8b41f16362f1f908662d41d8a36204fd19722be
Jordan Larock <jordan@larock.ca>
2018-12-03 15:10:04 -0500

    Post requests are happening
    Changed it to PUT since POST wasn't providing any output
    haven't test the PUT yet.

--- Computing In AR - Final Project/Assets/Scripts/UIManager.cs
+++ Computing In AR - Final Project/Assets/Scripts/UIManager.cs
@@ -1,4 +1,9 @@
 ﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Net;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,7 +19,46 @@ public class UIManager : MonoBehaviour
 
     public void LoadPhotoToTexture(Texture2D texture)
     {
+        Debug.Log("LPTT photo start");
         gameObjDisplayPhoto.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", texture);
+
+        string json = @"{
+                'requests':[
+            {
+      'image':{
+        'source':{
+          'imageUri':
+            'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+        }
+      },
+      'features':[
+        {
+          'type':'LOGO_DETECTION',
+          'maxResults':1
+        }
+      ]
+    }
+  ]
+}";
+
+        Debug.Log("post request start");
+        
+
+
+        using (UnityWebRequest www = UnityWebRequest.Put("https://vision.googleapis.com/v1/images:annotate", json))
+        {
+            www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("OK, response:");
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
     }
 
     public void PlayVideo()
